7aabe70a695a5c353290e93ed5863b24b0ab6543
fajar <fajar23.septianto@gmail.com>
2026-04-07 22:51:28 +0700

    day 2 belajar typescript. yang dilakukan adalah bikin edit dan store function

--- app.js
+++ app.js
@@ -15,15 +15,45 @@ function show(id) {
     const getSpesificData = mybook.find((book) => book.id === id);
     if (!getSpesificData) {
         console.log('data tidak ditemukan');
+        return;
     }
-    const data = [
-        {
-            pesan: "menampilkan data",
-            kode: 200,
-            data: getSpesificData
-        },
-    ];
-    console.log(JSON.stringify(data));
+    const data = {
+        pesan: "menampilkan data",
+        kode: 200,
+        data: getSpesificData
+    };
+    console.log(data);
 }
 show(1);
+function store() {
+    const data = {
+        id: 4,
+        name: "sejarah filsafat barat",
+        author: "bertrant russel"
+    };
+    const saveData = mybook.push(data);
+    const response = {
+        pesan: "input data berhasil",
+        kode: 200,
+        data: saveData
+    };
+    console.log(JSON.stringify(response));
+}
+store();
+function edit(id, newName, newAuthor) {
+    const getSpesificData = mybook.find((book) => book.id === id);
+    if (!getSpesificData) {
+        console.log('data tidak ditemukan');
+        return;
+    }
+    getSpesificData.name = newName;
+    getSpesificData.author = newAuthor;
+    const response = {
+        pesan: "berhasil mengubah data",
+        kode: 200,
+        data: getSpesificData
+    };
+    console.log(response);
+}
+edit(1, "funiculi funicula", 'toshikazu');
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -21,15 +21,48 @@ function show(id: number): void {
     const getSpesificData = mybook.find((book) => book.id === id)
     if (!getSpesificData) {
         console.log('data tidak ditemukan')
+        return;
     }
-    const data: apiResponses<typeof getSpesificData> = [
-        {
-            pesan: "menampilkan data",
-            kode: 200,
-            data: getSpesificData
-        },
+    const data: apiResponses<typeof getSpesificData> =
+    {
+        pesan: "menampilkan data",
+        kode: 200,
+        data: getSpesificData
+    };
 
-    ]
-    console.log(JSON.stringify(data))
+
+    console.log(data)
 }
 show(1)
+function store() {
+    const data = {
+        id: 4,
+        name: "sejarah filsafat barat",
+        author: "bertrant russel"
+    }
+    const saveData = mybook.push(data);
+    const response: apiResponses<typeof saveData> = {
+        pesan: "input data berhasil",
+        kode: 200,
+        data: saveData
+    }
+    console.log(JSON.stringify(response))
+}
+store()
+function edit(id: number, newName: string, newAuthor: string) {
+    const getSpesificData = mybook.find((book) => book.id === id)
+    if (!getSpesificData) {
+        console.log('data tidak ditemukan')
+        return;
+    }
+    getSpesificData.name = newName;
+    getSpesificData.author = newAuthor;
+    const response: apiResponses<typeof getSpesificData> = {
+        pesan: "berhasil mengubah data",
+        kode: 200,
+        data: getSpesificData
+    }
+    console.log(response);
+}
+
+edit(1, "funiculi funicula", 'toshikazu');
