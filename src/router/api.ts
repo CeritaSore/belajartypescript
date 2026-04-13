184ae8d0aedf0dbbe64456816476ada3906f7368
fajar <fajar23.septianto@gmail.com>
2026-04-13 14:17:31 +0700

    day 2: menggunakan orm dan migration untuk crud

--- src/router/api.ts
+++ src/router/api.ts
@@ -1,10 +1,13 @@
 import express, { type Request, type Response } from "express";
 import BookController from "../controller/BooksController.js";
+import { body } from "express-validator";
 export const router = express();
 router.use(express.json());
 router.get("/", (request: Request, response: Response) => {
   response.json("halo");
 });
-router.get("/books", BookController.index);
-router.get("/books/:id", BookController.show);
+router.get("/books", body('name'), BookController.index);
 router.post("/books/create", BookController.store);
+router.get("/books/:id", BookController.show);
+router.patch("/books/:id/update", BookController.update);
+router.delete("/books/:id/delete", BookController.destroy);
